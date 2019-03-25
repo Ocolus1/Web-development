b22a7287cf6afe88517d728ba7449721062b2fc6
Ocolus1 <47501949+Ocolus1@users.noreply.github.com>
2019-03-25 09:56:18 +0100

    Flask project
    Created a simple web page

---
+++ flask_project/data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article one',
+            'body':'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex maxime excepturi ad, ea deserunt, ut reiciendis, iusto aliquid ab aliq',
+            'author': 'Paul Awolesi',
+            'create_date':'3-25-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article two',
+            'body':'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex maxime excepturi ad, ea deserunt, ut reiciendis, iusto aliquid ab aliq',
+            'author': 'John Doe',
+            'create_date':'3-25-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article three',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex maxime excepturi ad, ea deserunt, ut reiciendis, iusto aliquid ab aliq',
+            'author': 'Paul Awolesi',
+            'create_date': '3-25-2019'
+        }
+    ]
+
+    return articles
